f7369a90337fa4ed443ab4476808bb1582ee6c2b
tomcrosscz <tomcrosscz@gmail.com>
2025-11-19 14:32:20 +0100

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,17 @@ import ssl
 class CORSRequestHandler(http.server.SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-       # Moderní SSL kontext        super().end_headers()
+        super().end_headers()
+
+# Vytvoření HTTP serveru
+httpd = http.server.HTTPServer(('0.0.0.0', 443), CORSRequestHandler)
+
+# Vytvoření SSL kontextu
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
 context.load_cert_chain(certfile='cert/selfsigned.crt', keyfile='cert/selfsigned.key')
 
+# Ovinutí socketu SSL kontextem
 httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
 
 print("Serving on https://0.0.0.0:443")
 httpd.serve_forever()
-
-# Spuštění HTTP serveru
-httpd = http.server.HTTPServer(('0.0.0.0', 443), CORSRequestHandler)
